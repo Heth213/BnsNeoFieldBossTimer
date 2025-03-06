51379386f7c65bd5080da5fb1992e41d319d4aaa
Heth <35584988+Heth213@users.noreply.github.com>
2025-03-07 00:22:09 +0300

    Update supabaseClient.js
    Signed-off-by: Heth <35584988+Heth213@users.noreply.github.com>

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.SUPABASE_URL; // paste supabase url here
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY; // paste supabase anon key here
+const SUPABASE_URL = 'https://wgwtxiryhaccadzbnise.supabase.co'; // paste supabase url here
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indnd3R4aXJ5aGFjY2FkemJuaXNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyOTMyNTMsImV4cCI6MjA1Njg2OTI1M30.ft3HT-U8yrB3JVO6X2EHL1x_SHWvn_QKRkMgIW-G6Mo'; // paste supabase anon key here
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
